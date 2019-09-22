67dd23a13ea357c9913f3190cd0575a260895aa5
James Leung <jleung128hk@gmail.com>
2019-09-22 08:45:02 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const helmet = require('helmet')
+const app = express();
+
+app.use(helmet())
+
+// Receive and parse json
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Send back json
+app.post("/ajax", (req, res) => {
+  console.log(req);
+  res.json("Test");
+});
+
+app.listen(8000);
+console.log("Server listening on port 8000");
